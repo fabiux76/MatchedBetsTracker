6fdc10724fccd3cc6a33b0b57ba89474e18bae9b
Fabio Adani <fabio.adani@gmail.com>
2018-08-14 22:23:08 +0200

    Add transaction controller

---
+++ MatchedBetsTracker/Controllers/TransactionController.cs
@@ -0,0 +1,36 @@
+﻿using MatchedBetsTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace MatchedBetsTracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public TransactionController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            var transactions = _context.Transactions
+                                    .Include(t => t.TransactionType)
+                                    .Include(t => t.BrokerAccount)
+                                    .ToList();
+
+            return View(transactions);
+        }
+    }
+}
