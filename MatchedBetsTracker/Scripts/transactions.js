5f131848a9f823f8f7c9c6cff7211cd5ac5b253c
Fabio Adani <fabio.adani@ynap.com>
2018-08-17 12:20:17 +0200

    Optimizing JQuery code and table

--- MatchedBetsTracker/Scripts/transactions.js
+++ MatchedBetsTracker/Scripts/transactions.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-    $("#transactions .js-change-status").on("click",
+    $("#transactions").on("click", ".js-change-status",
         function () {
+            var button = $(this);
+
             bootbox.confirm("Are you sure you want to change status?",
                 function(result) {
                     if (result) {
                         $.ajax({
-                            url: "/api/transactions/UpdateTransactionValidationStatus?id=" + $(this).attr("data-transaction-id") + "&isValid=true",
+                            url: "/api/transactions/UpdateTransactionValidationStatus?id=" + button.attr("data-transaction-id") + "&isValid=true",
                             method: "PUT",
                             success: function () {
                                 console.log("Changed status");
