f218c29f27589508de9c11312c9f5349a770e59a
Fabio Adani <fabio.adani@ynap.com>
2018-08-17 12:04:27 +0200

    Extending to all views with transactions list

--- MatchedBetsTracker/App_Start/BundleConfig.cs
+++ MatchedBetsTracker/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MatchedBetsTracker
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/mycode").Include(
+                "~/Scripts/transactions.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
--- MatchedBetsTracker/Scripts/transactions.js
+++ MatchedBetsTracker/Scripts/transactions.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+    $("#transactions .js-change-status").on("click",
+        function () {
+            if (confirm("Are you sure you want to change status?")) {
+                $.ajax({
+                    url: "/api/transactions/UpdateTransactionValidationStatus?id=" + $(this).attr("data-transaction-id") + "&isValid=true",
+                    method: "PUT",
+                    success: function () {
+                        console.log("Changed status");
+                    }
+                });
+            }
+        });
 });
